a0fc7bd424676eba69801201f2e1972d0d1e658a
Anhelina Gordieieva <angelina.gordeeva10@gmail.com>
2024-10-30 19:39:00 +0200

    Добавьте файлы проекта.

---
+++ lesson13.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int function(char *mas) {
+    try {
+        long long res = 0, ten = 1;
+        for (short i = 0; i < sizeof(mas) / sizeof(mas[0]); i++) {
+            res += static_cast<long long>(mas[i]) * ten;
+            ten *= 10;
+        }
+        if (-2147483648 > res || res > 2147483648) {
+            throw - 1;
+        }
+        return res;
+    }
+    catch (int a) {
+        cout << "Error";
+        return a;
+    }
+}
+int main()
+{
+    char mas[11];
+    cin >> mas;
+    cout<<function(mas);
+}
+
